385587e2925cab7fc0af145d78dce42745ce44cd
Dušan Simić <dusan.simic1810@gmail.com>
2025-06-02 13:14:49 +0200

    feat: Bump bruno version

--- build_files/dev/50_bruno.sh
+++ build_files/dev/50_bruno.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 ### Install Bruno
 
-BRUNO_VER="2.3.0"
+BRUNO_VER="2.4.0"
 
 SRC_URL="https://github.com/usebruno/bruno/releases/download/v${BRUNO_VER}/bruno_${BRUNO_VER}_x86_64_linux.rpm"
 
